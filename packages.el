624851efd123ca8376c1109ea52decb91e9f3e3f
Albert Likaj <dagobah1528@gmail.com>
2024-12-11 23:14:19 +0100

    Add packages projectile and which-key

--- packages.el
+++ packages.el
@@ -9,4 +9,15 @@
   :commands (magit-status)
   :bind (("C-x g" . magit-status)))
 
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode t))
+
 ;;; packages.el ends here
