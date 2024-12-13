ea310d27f50bbb08f20ff1aaf9e3983c0e506c51
Albert Likaj <dagobah1528@gmail.com>
2024-12-13 22:32:33 +0100

    Add helm. Change key bindings.

--- packages.el
+++ packages.el
@@ -7,17 +7,25 @@
 (use-package magit
   :ensure t
   :commands (magit-status)
-  :bind (("C-x g" . magit-status)))
+  :bind (("C-l v" . magit-status)))
 
 (use-package projectile
   :ensure t
   :diminish projectile-mode
   :config (projectile-mode)
   :bind-keymap
-  ("C-c p" . projectile-command-map))
+  ("C-l p" . projectile-command-map))
 
 (use-package which-key
   :ensure t
   :config (which-key-mode t))
+
+(use-package helm
+  :ensure t
+  :init
+  (helm-mode t)
+  (progn (setq helm-buffers-fuzzy-matching t))
+  :bind
+  (("M-x" . helm-M-x)))
 
 ;;; packages.el ends here
