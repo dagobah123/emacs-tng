8ebb4c282876ec1393c579f91752c0495a1f32c1
Albert Likaj <dagobah1528@gmail.com>
2024-12-15 21:28:50 +0100

    Add custom prefix keymap

--- keybindings.el
+++ keybindings.el
@@ -10,4 +10,9 @@
 (define-key my-prefix-map (kbd "b") 'bookmark-bmenu-list)
 (define-key my-prefix-map (kbd "s") 'my-grep-in-project2)
 
+(defvar my-prefix-map-ring (make-sparse-keymap) "My custom prefix keymap")
+(define-key my-prefix-map (kbd "r") my-prefix-map-ring)
+(define-key my-prefix-map-ring (kbd "m") 'helm-all-mark-rings)
+(define-key my-prefix-map-ring (kbd "k") 'helm-show-kill-ring)
+
 ;;; keybindings.el ends here
