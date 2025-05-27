c11177499330df68f82ce7575e0080cf0bcf6562
Albert Likaj <dagobah1528@gmail.com>
2025-05-27 22:41:15 +0200

    Add kbd for counsel-projectile-git-grep

--- keybindings.el
+++ keybindings.el
@@ -24,6 +24,7 @@
 (define-key my-prefix-map-search (kbd "2") #'my-grep-in-project-multiple-matches-in-line)
 (define-key my-prefix-map-search (kbd "3") #'my-grep-in-project-exact)
 (define-key my-prefix-map-search (kbd "a") #'ag)
+(define-key my-prefix-map-search (kbd "c") #'counsel-projectile-git-grep)
 (define-key my-prefix-map-search (kbd "o") #'occur)
 (define-key my-prefix-map-search (kbd "s") #'swiper-all)
 
