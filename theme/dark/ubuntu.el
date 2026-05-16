24f4a9ba15b5ccfb86a9d713678e76a3c703f6ad
Albert Likaj <dagobah1528@gmail.com>
2026-05-16 12:58:30 +0200

    Add missing ubuntu.el

---
+++ theme/dark/ubuntu.el
@@ -0,0 +1,23 @@
+;; ubuntu --- theme  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(load-file "~/.emacs.d/theme/common/variables.el")
+
+(setq background "#300a24")
+(setq text-comment "#555555")
+(setq text-constant "#969696")
+(setq text-function "#26a269")
+(setq text-keyword "#5c5c5c")
+(setq text-line-number "#595959")
+(setq text-link "#9d9d9d")
+(setq text-low "#666666")
+(setq text-lower "#444444")
+(setq text-mode-line-inactive "#444444")
+(setq text-normal "#ffffff")
+(setq text-preprocessor "#696969")
+(setq text-type "#12488b")
+(setq text-variable "#aaaaaa")
+(setq text-whitespace "#4d4d4d")
+
+;;; ubuntu.el ends here
